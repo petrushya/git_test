cd99143fe5bff72f4a039f4691cc1b9b91c377a6
petrushya <108490184+petrushya@users.noreply.github.com>
2024-03-18 07:53:54 +0300

    changes game logics with buttons in game.js, adds victory condition in index.html

--- js/game.js
+++ js/game.js
@@ -1,76 +1,54 @@
 
-// array for computer
-const computerArr = ["rock", "scissors", "paper"];
+const playBlock = document.querySelector('#playGame');
+const playScore = document.querySelector('#playScore');
+const para = playScore.querySelector('p');
 
-// selecting a random array element between the min and max values the way I liked it
-// computerRandom = Math.floor(min + Math.random() * (max - min + 1))
-
+const computerArr = ["rock", "paper", "scissors"];
 function getComputerChoice(){return computerArr[Math.floor(Math.random()*3)]}
-
-// check the user's prompt field entry and get the value
-
-function getPlayerSelection(){
-  let playerString = prompt("Please write your choice: rock, scissors or paper.");
-  if(playerString && computerArr.includes(playerString.toLowerCase())){
-    return playerString = playerString.toLowerCase();
-  }else{
-     getPlayerSelection();
-  }
-}
-
-// function describing one round with return of the result
 
 let roundResult = "";
 
-function playRound(playerSelection, computerSelection){
-	switch(`${playerSelection}-${computerSelection}`){
-		case 'rock-scissors':
-		case 'scissors-paper':
-		case 'paper-rock':
+function playRound(playSelect, compSelect){
+  switch(`${playSelect}-${compSelect}`){
+    case 'rock-scissors':
+    case 'scissors-paper':
+    case 'paper-rock':
       return roundResult = "playerWon";
-			break;
-		case 'rock-rock':
-		case 'paper-paper':
-		case 'scissors-scissors':
-			return roundResult = "";
-			break;
-		default:
+      break;
+    case 'rock-rock':
+    case 'paper-paper':
+    case 'scissors-scissors':
+      return roundResult = "";
+      break;
+    default:
       return roundResult = "computerWon";
-	}
-}
+  };
+};
 
-// main function for the game
+let playerCounter = 0;
+let compCounter = 0;
 
-function playGame(){
-  let playerCounter = 0;
-  let compCounter = 0;
-  for(let index = 0; index < 5; index++){
-    const computerSelection = getComputerChoice();
-    const playerSelection = getPlayerSelection();
-		const playerRoundWon = `You Won! ${playerSelection} beats ${computerSelection}`;
-		const computerRoundWon = `You Lose! ${computerSelection} beats ${playerSelection}`;
-		const roundDeadHeat = `Dead Heat! ${computerSelection} equals ${playerSelection}`;
-    playRound(playerSelection, computerSelection);  
-    if(roundResult === "playerWon"){
-    	console.log(playerRoundWon);
+playBlock.addEventListener('click', (event) => {
+  let target = event.target;
+  const playerSelection = target.id;
+
+  if(para.textContent !== '') para.textContent = '';
+
+  const computerSelection = getComputerChoice();
+  playRound(playerSelection, computerSelection);
+ 
+  switch(roundResult){
+    case 'playerWon':
+      para.textContent = `You Won the round! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`;
       playerCounter += 1;
-    }else if(roundResult === "computerWon"){
-    	console.log(computerRoundWon);
+      break;
+    case 'computerWon':
+      para.textContent = `You Lose the round! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`;
       compCounter += 1;
-    }else{
-    	console.log(roundDeadHeat);
-    }
-  }
-  if(playerCounter > compCounter) {
-    console.log(`You Won the Game! Game count: ${playerCounter} - ${compCounter}`);
-  }else if(playerCounter < compCounter){
-    console.log(`You Lose the Game! Game count: ${playerCounter} - ${compCounter}`);
-  }else{
-    console.log(`Dead Heat, Game count: ${playerCounter} - ${compCounter}`);
-  }
-}
+      break;
+    default:
+      para.textContent = `The round was a Draw! ${computerSelection.toUpperCase()} equals ${playerSelection.toUpperCase()}`;
+  };
+});
 
-// play on
-
-//playGame();
 
